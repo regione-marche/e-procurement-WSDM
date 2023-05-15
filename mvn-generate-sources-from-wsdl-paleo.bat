a9c129ba1097f06a2d12c032c4f49d719b566f7f
Alvise Gorinati <alvise.gorinati@gmail.com>
2023-05-15 16:36:03 +0200

    Sistemati i nuovi sorgenti

--- mvn-generate-sources-from-wsdl-paleo.bat
+++
@@ -1,1 +0,0 @@
-mvn clean generate-sources -file pom-generate-sources-from-wsdl-paleo.xml -Djavax.net.ssl.trustStore=paleo.jks -Djavax.net.ssl.trustStorePassword=maggioli -Djavax.net.ssl.keyStoreType=jks 
